dc8edf24a3f823fabb2c5dfd192c63bbbf93b512
Dmitriy Savchenko <login.was.here@gmail.com>
2013-09-07 19:30:26 +0300

    Add basic pixel clicker functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,10 @@
 angular.module('clientApp')
     .controller('MainCtrl', ['$scope', function ($scope) {
         $scope.title = 'Pixel Clicker';
+
+        $scope.numberOfClicks = 0;
+
+        $scope.click = function(){
+            $scope.numberOfClicks++;
+        };
     }]);
