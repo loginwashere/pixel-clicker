67c03f150831155152ee1981faacf2b2f85db13d
Dmitriy Savchenko <login.was.here@gmail.com>
2013-09-08 19:19:22 +0300

    Redesign shop and pixel view, start adding buy and sell handlers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,13 +4,9 @@ angular.module('clientApp')
     .controller('MainCtrl', ['$scope', 'game', function ($scope, game) {
         $scope.title = 'Pixel Clicker';
 
-        game.init();
-
-        $scope.clock = game.getClock();
-
         game.loop($scope);
 
         $scope.numberOfClicks = game.getNumberOfClicks();
 
-        $scope.click = game.clickPixel($scope);
+        $scope.clickPixel = game.clickPixel($scope);
     }]);
--- app/scripts/services/game.js
+++ app/scripts/services/game.js
@@ -1,91 +1,35 @@
 'use strict';
 
 angular.module('clientApp')
-    .service('game', function Game() {
-        // AngularJS will instantiate a singleton by calling "new" on this function
-        var self = this;
+    .service('game', function Game(Products) {
+        return new (function (Products) {
+            var self = this;
 
-        this.init = function () {
-            this.clock = 0;
             this.numberOfClicks = 0;
-            this.products = [
-                {
-                    "name": "cursor",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 100
-                },
-                {
-                    "name": "granny",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 200
-                },
-                {
-                    "name": "farm",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 200
-                },
-                {
-                    "name": "factory",
-                    "quantity": 0,
-                    "icon": "glyphicon-star-empty",
-                    "currentPrice": 200
-                },
-                {
-                    "name": "delivery",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 200
-                },
-                {
-                    "name": "lab",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 200
-                },
-                {
-                    "name": "portal",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 200
-                },
-                {
-                    "name": "time machine",
-                    "quantity": 0,
-                    "icon": "glyphicon-star",
-                    "currentPrice": 200
+
+            this.products = Products.getProducts();
+
+            this.loop = function (scope) {
+                setInterval(function () {
+                    scope.$apply(function () {
+
+                    });
+                }, 1000 / 60);
+            };
+
+            this.clickPixel = function (scope) {
+                return function () {
+                    self.numberOfClicks++;
+                    scope.numberOfClicks = self.numberOfClicks;
                 }
-            ];
-        };
+            };
 
-        this.loop = function (scope) {
-            setInterval(function () {
-                scope.$apply(function () {
-                    self.clock += 111111111111111111;
-                    scope.clock = self.clock;
-                });
-            }, 1000 / 60);
-        };
+            this.getNumberOfClicks = function () {
+                return this.numberOfClicks;
+            };
 
-        this.clickPixel = function (scope) {
-            return function () {
-                self.numberOfClicks++;
-                console.log(self.clock);
-                scope.numberOfClicks = self.numberOfClicks;
-            }
-        };
-
-        this.getClock = function () {
-            return this.clock;
-        };
-
-        this.getNumberOfClicks = function () {
-            return this.numberOfClicks;
-        };
-
-        this.getProducts = function () {
-            return this.products;
-        };
-    });
+            this.getProducts = function () {
+                return this.products;
+            };
+        })(Products);
+    }, {$inject: ['Products']});
