9222b46d7c40e1af5148f543a2403d2974d09e96
Dmitriy Savchenko <login.was.here@gmail.com>
2013-11-14 23:50:53 +0200

    Add viewing stored game details

--- app/scripts/controllers/navigation/menu.js
+++ app/scripts/controllers/navigation/menu.js
@@ -3,8 +3,15 @@ define(['../module', 'storejs'], function (controllers, store) {
 
     controllers.controller('MenuCtrl', ['$scope', function ($scope) {
         $scope.content = 'Menu';
+        $scope.gameContents = store.get('game');
         $scope.saveGame = function(game) {
-            store.set('game', game)
+            store.set('game', game);
+            $scope.gameContents = store.get('game');
         };
+        $scope.loadGame = function() {
+            var game = store.get('game');
+            console.log(game);
+            $scope.game = game;
+        }
     }]);
 });
